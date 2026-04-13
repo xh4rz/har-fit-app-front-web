07c9fbce8d77dcdba9ed1c5158946c6fc4f9bdeb
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-04-12 21:47:27 -0500

    feat: setup shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Inter } from 'next/font/google';
 import './globals.css';
 import { AppContextProvider } from '@/context';
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -29,7 +29,15 @@ export default function RootLayout({
 	return (
 		<html
 			lang="en"
-			className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+			className={cn(
+				'h-full',
+				'antialiased',
+				geistSans.variable,
+				geistMono.variable,
+				'font-sans',
+				inter.variable,
+				'dark'
+			)}
 		>
 			<body className="min-h-full flex flex-col">
 				<AppContextProvider>{children}</AppContextProvider>
