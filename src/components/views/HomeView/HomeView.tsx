2a0a83a1f57d45b605f2af578e78944c1a081743
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-05-20 03:07:03 -0500

    feat: add badges app store and google play store

--- src/components/views/HomeView/HomeView.tsx
+++ src/components/views/HomeView/HomeView.tsx
@@ -1,17 +1,19 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
+import { useAuthStore } from '@/modules/auth/store/useAuthStore';
 import { HomeItem } from '@/components/molecules';
 import { Card } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
-import { useAuthStore } from '@/modules/auth/store/useAuthStore';
+import { Skeleton } from '@/components/ui/skeleton';
+import { AppQRCode } from '@/components/atoms';
 import {
 	BarbellIcon,
 	DownloadSimpleIcon,
 	UserIcon
 } from '@phosphor-icons/react';
-import { Skeleton } from '@/components/ui/skeleton';
-import { AppQRCode } from '@/components/atoms';
+import { URLS } from '@/lib/constants/urls';
 
 export const HomeView = () => {
 	const { user } = useAuthStore((state) => state);
@@ -41,30 +43,57 @@ export const HomeView = () => {
 					</div>
 					<div className="w-full">
 						<Image
+							priority
 							loading="eager"
 							src="/images/banner-har-fit.png"
 							alt="home banner"
 							width={500}
 							height={500}
 							className="w-lg "
-							priority
 						/>
 					</div>
 				</div>
 			</div>
 			<div className="px-4 sm:px-8 lg:px-12 py-8 flex flex-col gap-4">
-				<div className="flex gap-20">
-					<HomeItem
-						icon={<DownloadSimpleIcon size={30} className="text-primary" />}
-						title="Download the HarFit mobile app"
-					/>
-
-					<AppQRCode
-						value="https://github.com/xh4rz/har-fit-app-front-mobile/releases/tag/v1.0.0"
-						size={100}
-					/>
+				<div className="flex flex-col xl:flex-row xl:items-center gap-6">
+					<div className="flex-1">
+						<HomeItem
+							icon={<DownloadSimpleIcon size={30} className="text-primary" />}
+							title="Download the HarFit mobile app"
+						/>
+					</div>
+					<div className="flex justify-center xl:flex-1">
+						<AppQRCode value={URLS.appDownload} size={120} />
+					</div>
+					<div className="flex flex-col sm:flex-row items-center justify-center gap-3 xl:flex-1">
+						<Link
+							href={URLS.appDownload}
+							target="_blank"
+							className="relative h-14 w-44"
+						>
+							<Image
+								fill
+								priority
+								src="/images/badge-google-play-store.svg"
+								alt="Get it on Google Play"
+								className="object-contain transition-transform hover:scale-105"
+							/>
+						</Link>
+						<Link
+							href={URLS.appDownload}
+							target="_blank"
+							className="relative h-14 w-44"
+						>
+							<Image
+								fill
+								priority
+								src="/images/badge-app-store.svg"
+								alt="Download on the App Store"
+								className="object-contain transition-transform hover:scale-105"
+							/>
+						</Link>
+					</div>
 				</div>
-
 				<Separator />
 				<HomeItem
 					icon={<UserIcon size={30} className="text-primary" />}
---
+++ src/lib/constants/urls.ts
@@ -0,0 +1,4 @@
+export const URLS = {
+	appDownload:
+		'https://github.com/xh4rz/har-fit-app-front-mobile/releases/tag/v1.0.0'
+};
